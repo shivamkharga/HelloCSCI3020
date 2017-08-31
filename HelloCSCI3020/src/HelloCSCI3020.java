c90aaac91200af19623582f7040df0e39bc4a565
Lon Smith <proflon5@gmail.com>
2017-08-31 12:22:21 -0500

    Added Goodbye Message

--- HelloCSCI3020/src/HelloCSCI3020.java
+++ HelloCSCI3020/src/HelloCSCI3020.java
@@ -5,7 +5,8 @@ public class HelloCSCI3020
 	public static void main(String[] args)
 	{
 		System.out.println("Hello CSCI 3020 ;)!");
-
+		Simple msg = new Simple("Goodbye");
+		System.out.println(msg);
 	}
 
 }
---
+++ HelloCSCI3020/src/Simple.java
@@ -0,0 +1,18 @@
+
+public class Simple
+{
+	private String data;
+
+	public Simple(String data)
+	{
+		super();
+		this.data = data;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Simple [data=" + data + "]";
+	}
+	
+}
